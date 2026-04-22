d81eeb1c372fee5eac102796d9c3cb92b8893395
mondedesgeeks-beep <mondedesgeeks@gmail.com>
2026-04-22 16:59:19 +0200

    Create  main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DiagCheck from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DiagCheck />
+  </React.StrictMode>
+)
